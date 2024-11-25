5ff2292aca2f8f50170acf3ab2f9ba0f1a698988
ilkeratik <ilkeratik35@gmail.com>
2024-11-25 18:00:32 +0000

    Add the watchlist v1

---
+++ app/src/main/java/com/ilkeratik/watchlist/ui/WellnessTask.kt
@@ -0,0 +1,25 @@
+package com.ilkeratik.watchlist.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+
+data class WellnessTask(val id: Int, val label: String)
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
