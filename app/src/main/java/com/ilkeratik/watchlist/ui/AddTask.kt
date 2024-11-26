4b063a0eb2e1ac20613827468674b9684ef1d59c
ilkeratik <ilkeratik35@gmail.com>
2024-11-26 17:05:30 +0000

    handling empty label

--- app/src/main/java/com/ilkeratik/watchlist/ui/AddTask.kt
+++ app/src/main/java/com/ilkeratik/watchlist/ui/AddTask.kt
@@ -3,8 +3,10 @@ package com.ilkeratik.watchlist.ui
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,20 +14,33 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ilkeratik.watchlist.data.WellnessTask
 
 @Composable
-fun StatefulAddTask(onSave: (WellnessTask) -> Unit, modifier: Modifier = Modifier) {
+fun StatefulAddTask(onSave:  (WellnessTask) -> Unit, modifier: Modifier = Modifier,showSnackBar: Boolean = false ) {
     var value by rememberSaveable { mutableStateOf("") }
+    if(showSnackBar){
+        Snackbar(containerColor = Color.LightGray, modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .padding(5.dp)) {
+            Text(text = "Task label cannot be empty", color = Color(0xFFB00020), fontWeight = FontWeight.Bold)
+        }
+    }
     StatelessAddTask(value, { wax-> value=wax }, onSave, modifier)
 }
 
 @Composable
 fun StatelessAddTask(input: String, onInputChange: (String) -> Unit, onSave: (WellnessTask)-> Unit, modifier: Modifier = Modifier) {
+
     Row (
-        modifier = Modifier.padding( 5.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
         verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
         horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceBetween) {
         OutlinedTextField(
--- app/src/main/java/com/ilkeratik/watchlist/ui/WellnessScreen.kt
+++ app/src/main/java/com/ilkeratik/watchlist/ui/WellnessScreen.kt
@@ -2,6 +2,10 @@ package com.ilkeratik.watchlist.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ilkeratik.watchlist.data.WellnessTasksList
@@ -10,8 +14,16 @@ import com.ilkeratik.watchlist.viewmodel.WellnessViewModel
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier,
                    wellnessViewModel: WellnessViewModel = viewModel()) {
+    var showError by rememberSaveable { mutableStateOf(false) }
     Column(modifier = modifier) {
-        StatefulAddTask(onSave = { newTask -> wellnessViewModel.add(newTask) })
+        StatefulAddTask(onSave = { newTask ->
+            if(newTask.label.isNotEmpty()){
+                wellnessViewModel.add(newTask)
+                showError = false
+            } else{
+                showError = true
+            } },
+            showSnackBar = showError)
         WellnessTasksList(list = wellnessViewModel.tasks,
             onCheckedTask = { task, checked ->
                 wellnessViewModel.changeTaskChecked(task, checked)
